e364606331d38c55d20f607e69722fe159d748d9
Daniel Cortes <dacortesn@ufpso.edu.co>
2022-04-21 19:59:29 -0500

    Segunda actualización de las clases parcial 1

--- src/test/java/com/supermercado/producto/ProductRepositoryTests.java
+++ src/test/java/com/supermercado/producto/ProductRepositoryTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,6 +22,7 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
+
     @Test
     public void testCreateProduct(){
         Product product = new Product();
@@ -33,48 +35,5 @@ public class ProductRepositoryTests {
 
         assertEquals(product.getName(), existProduct.getName());
     }
-    public void testGetproduct(){
-        Product product = new Product();
-        product.setName("Leche");
-        product.setDescription("Lacteo");
 
-        Product savedProduct = repository.save(product);
-
-        Product existProduct = entityManager.find(Product.class, savedProduct.getId());
-
-        assertEquals(product.getName(), existProduct.getName());
-    }
-    public void testListProduct(){
-        Product product = new Product();
-        product.setName("Leche");
-        product.setDescription("Lacteo");
-
-        Product savedProduct = repository.save(product);
-
-        Product existProduct = entityManager.find(Product.class, savedProduct.getId());
-
-        assertEquals(product.getName(), existProduct.getName());
-    }
-    public void testEditProduct(){
-        Product product = new Product();
-        product.setName("Leche");
-        product.setDescription("Lacteo");
-
-        Product savedProduct = repository.save(product);
-
-        Product existProduct = entityManager.find(Product.class, savedProduct.getId());
-
-        assertEquals(product.getName(), existProduct.getName());
-    }
-    public void testDeleteProduct(){
-        Product product = new Product();
-        product.setName("Leche");
-        product.setDescription("Lacteo");
-
-        Product savedProduct = repository.save(product);
-
-        Product existProduct = entityManager.find(Product.class, savedProduct.getId());
-
-        assertEquals(product.getName(), existProduct.getName());
-    }
 }
